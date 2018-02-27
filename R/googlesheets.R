554e1eca5f2e973e5a4db333bfdfd9debae6bab3
murray.cadzow <murray.cadzow@otago.ac.nz>
2018-02-28 07:29:00 +1300

    load or install librarires - if missing

---
+++ R/googlesheets.R
@@ -0,0 +1,21 @@
+#' Googlesheets demo
+
+
+#' load the libraries needed
+#' If they aren't installed, install them
+if(!require(here)){
+  install.packages("here")
+}
+if(!require(googlesheets)){
+  install.packages('googlesheets')
+}
+if(!require(kableExtra)){
+  install.packages("kableExtra")
+}
+if(!require(dplyr)){
+  install.packages("dplyr")
+}
+if(!require(ggplot2)){
+  install.packages("ggplot2")
+}
+
