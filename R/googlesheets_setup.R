5e912d6ffb5d0db06cb788fcd7661b6c8e3eabd0
murray.cadzow <murray.cadzow@otago.ac.nz>
2018-02-28 11:57:11 +1300

    download and authorise a token to work with your google account

--- R/googlesheets_setup.R
+++ R/googlesheets_setup.R
@@ -1,10 +1,13 @@
 #' Googlesheets demo
-
+#' script is designed to be run in an 'interactive' session, not sourced
 
 #' load the libraries needed
 #' If they aren't installed, install them
 if(!require(here)){
   install.packages("here")
+}
+if(!require(usethis)){
+  install.packages('usethis')
 }
 if(!require(googlesheets)){
   install.packages('googlesheets')
@@ -19,3 +22,13 @@ if(!require(ggplot2)){
   install.packages("ggplot2")
 }
 
+
+#' Need to setup the google authorisation file (needs to be done interactively)
+#' see https://github.com/jennybc/googlesheets/blob/master/vignettes/managing-auth-tokens.Rmd for extra details
+library(googlesheets)
+token <- gs_auth(cache = FALSE)
+gd_token()
+saveRDS(token, file = "googlesheets_token.rds")
+
+#' now make sure to add "googlesheets_token.rds" into your .gitignore
+usethis::edit_git_ignore(scope = 'project')
